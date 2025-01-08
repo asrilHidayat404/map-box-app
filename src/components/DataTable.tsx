c5ccf8b4730406f7ae58dba8ffb4324b9f7a58fb
asrilHidayat404 <asril.code404@gmail.com>
2025-01-08 14:58:42 +0700

    complete map task

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,35 +1,47 @@
-"use client"
+"use client";
 
-import { deleteCoordinate } from "@/action/deleteCoordinate"
-import { startTransition, useActionState } from "react"
+import { deleteCoordinate } from "@/action/deleteCoordinate";
+import { startTransition, useActionState } from "react";
 
-const DataTable = ({dataMap}) => {
-    const [error, action, loading] = useActionState(deleteCoordinate)
+type DataMap = {
+  id: number;
+  latitude: string;
+  longitude: string;
+  keterangan: string;
+  createdAt: Date;
+};
+
+const DataTable = ({ dataMap }: {dataMap: DataMap[]}) => {
+  console.log(dataMap);
+
+  const [error, action, loading] = useActionState(deleteCoordinate);
 
   return (
     <table className="min-w-full table-auto bg-white">
-    <thead className="bg-gray-800 text-white">
-      <tr>
-        <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
-          ID
-        </th>
-        <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
-          Latitude
-        </th>
-        <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
-          Longitude
-        </th>
-        <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
-          Keterangan
-        </th>
-        <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+      <thead className="bg-gray-800 text-white">
+        <tr>
+          <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+            ID
+          </th>
+          <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+            Latitude
+          </th>
+          <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+            Longitude
+          </th>
+          <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+            Keterangan
+          </th>
+          <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+            Dibuat
+          </th>
+          <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
             Action
-        </th>
-      </tr>
-    </thead>
-    <tbody className="text-gray-700 divide-y divide-gray-200">
-      {
-        dataMap?.map( map => {
+          </th>
+        </tr>
+      </thead>
+      <tbody className="text-gray-700 divide-y divide-gray-200">
+        {dataMap?.map((map) => {
           return (
             <tr className="hover:bg-gray-100" key={map.id}>
               <td className="py-3 px-4">{map.id}</td>
@@ -37,25 +49,33 @@ const DataTable = ({dataMap}) => {
               <td className="py-3 px-4">{map.longitude}</td>
               <td className="py-3 px-4">{map.keterangan}</td>
               <td className="py-3 px-4">
-                <button 
-                    className='bg-red-500 px-3 py-1 rounded-lg text-white font-semibold'
-                    onClick={ () => {
-                            startTransition(() => {
-                                action(map.id)
-                            })
-                        }
-                    }
+                {map.createdAt.toLocaleDateString("id-ID", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric", 
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  second: '2-digit',
+                })}
+              </td>
+              <td className="py-3 px-4">
+                <button
+                  className="bg-red-500 px-3 py-1 rounded-lg text-white font-semibold"
+                  onClick={() => {
+                    startTransition(() => {
+                      action(map.id);
+                    });
+                  }}
                 >
-                    Delete
+                  Delete
                 </button>
               </td>
             </tr>
-          )
-        })
-      }
-    </tbody>
-  </table>
-  )
-}
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
-export default DataTable
+export default DataTable;
--- src/layouts/Map.tsx
+++ src/layouts/Map.tsx
@@ -15,11 +15,11 @@ const Map = () => {
         // Set access token
         mapboxgl.accessToken =
           "pk.eyJ1IjoibG9uamhlciIsImEiOiJjbHlxcWsxNnMxNW5mMmpzNDZsZGZ6Z3RpIn0.Ec929E9mYpzsMfKqdPIuOQ";
-
+          // -7.06432184554814, 113.67460428315444
         const map = new mapboxgl.Map({
           container: mapContainer.current,
           style: "mapbox://styles/mapbox/streets-v11",
-          center: [-122.420679, 37.772537],
+          center: [113.67460428315444, -7.06432184554814],
           zoom: 13,
         });
 
@@ -35,68 +35,72 @@ const Map = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center space-y-4 p-4">
+    <div className="flex items-center space-x-4 p-4">
       <div
         ref={mapContainer}
-        className="w-full h-[500px] border rounded-md shadow-lg"
+        className="w-full h-[700px] border rounded-md shadow-lg"
       />
-      <form
-        action={action}
-        className="flex flex-col w-full max-w-md bg-white p-6 rounded-md shadow-lg space-y-4"
-      >
-        <div className="flex flex-col">
-          <label
-            htmlFor="latitude"
-            className="text-gray-700 font-medium mb-2"
-          >
-            Latitude
-          </label>
-          <input
-            type="text"
-            name="latitude"
-            value={coordinate.lat} // Nilai diambil dari state
-            readOnly
-            className="border border-gray-300 rounded-md p-2 text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label
-            htmlFor="longitude"
-            className="text-gray-700 font-medium mb-2"
-          >
-            Longitude
-          </label>
-          <input
-            type="text"
-            name="longitude"
-            value={coordinate.lng} // Nilai diambil dari state
-            readOnly
-            className="border border-gray-300 rounded-md p-2  text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label
-            htmlFor="keterangan"
-            className="text-gray-700 font-medium mb-2"
-          >
-            Keterangan
-          </label>
-          <input
-            type="text"
-            name="keterangan"
-            placeholder="Masukkan keterangan"
-            className="border border-gray-300 rounded-md p-2 
-            text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-        </div>
-        <button
-          type="submit"
-          disabled={loading}
-          className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
+      <div className="w-[800px] h-[700px] flex flex-col justify-between bg-white p-6 rounded-md shadow-lg ">
+        <h1 className="text-black text-center mt-10 text-3xl">Insert New Location</h1>
+        <img src="/logoUA.png" alt="" width={150} className="mx-auto" />
+        <form
+          action={action}
+          className="flex flex-col mx-auto w-full max-w-md space-y-4"
         >
-          {loading ? "Submitting" : "Submit"}
-        </button>
-      </form>
+          <div className="flex flex-col">
+            <label
+              htmlFor="latitude"
+              className="text-gray-700 font-medium mb-2"
+            >
+              Latitude:
+            </label>
+            <input
+              type="text"
+              name="latitude"
+              value={coordinate.lat} // Nilai diambil dari state
+              readOnly
+              className="border border-gray-300 rounded-md p-2 text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="longitude"
+              className="text-gray-700 font-medium mb-2"
+            >
+              Longitude:
+            </label>
+            <input
+              type="text"
+              name="longitude"
+              value={coordinate.lng} // Nilai diambil dari state
+              readOnly
+              className="border border-gray-300 rounded-md p-2  text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="keterangan"
+              className="text-gray-700 font-medium mb-2"
+            >
+              Keterangan:
+            </label>
+            <input
+              type="text"
+              name="keterangan"
+              placeholder="Masukkan keterangan"
+              className="border border-gray-300 rounded-md p-2 
+              text-black focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={loading}
+            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
+          >
+            {loading ? "Submitting" : "Submit"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
